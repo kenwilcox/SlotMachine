3113e48db73f25ec3d366c76d2827e12519d8ab1
kenwilcox <wilcoxk@gmail.com>
2014-12-31 23:24:12 -0700

    Added Slot struct

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Kenneth Wilcox on 12/31/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+  var value = 0
+  var image = UIImage(named: "Ace")
+  var isRed = true
+}
