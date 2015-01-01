78268fc0f948cbe310599d02b4309c3adb6dca6d
kenwilcox <wilcoxk@gmail.com>
2015-01-01 11:23:38 -0700

    Added SlotBrain

---
+++ SlotMachine/SlotBrain.swift
@@ -0,0 +1,23 @@
+//
+//  SlotBrain.swift
+//  SlotMachine
+//
+//  Created by Kenneth Wilcox on 1/1/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+
+class SlotBrain {
+  class func computeWinnings (slots: [[Slot]]) -> Int {
+    var winnings = 0
+    var flushWinCount = 0
+    var threeOfAKindWinCount = 0
+    var straightWinCount = 0
+    for slotRow in slots {
+      
+    }
+    return winnings
+  }
+  
+}
